a351f4c7d47bb26c09a5a7a96959d59a1e414a71
Eddie <heungryuncho@naver.com>
2022-11-29 09:34:47 +0900

    221129

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -20,17 +20,20 @@ public class AppConfig {
 
     @Bean // -> 스프링 컨테이너에 등록
     public MemberService memberService(){
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
 
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService(){
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(),
                 discountPolicy());
     }
